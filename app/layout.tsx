3df157dae3f271a2f73bdd51e4cc799830adfd4f
Dhruv Deora <dhruvdeora31@gmail.com>
2025-08-09 17:33:08 +0530

    fixed layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps) {
   return (
-    <html lang="en" className={`motion-safe:scroll-smooth 2xl:text-[24px] ${loraFont.variable} ${dmSansFont.variable} ${ibmPlexMonoFont.variable} font-dm-sans`}>
+    <html lang="en" className={`motion-safe:scroll-smooth  ${loraFont.variable} ${dmSansFont.variable} ${ibmPlexMonoFont.variable} font-dm-sans`}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
